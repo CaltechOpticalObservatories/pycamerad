d0683d9cce8e474c2a2fce72e9cd446d0b879d91
Don Neill <neilljd@gmail.com>
2024-11-21 17:59:17 -0800

    Initial effort at Camera class in camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,22 @@
 """ Camera class """
+import os
+import json
 from interface import Interface
 from exposure import Exposure
+import version
+
+default_host_config = os.path.join(version.CONFIG_DIR, "hosts.json")
 
 class Camera(Interface, Exposure):
+
+    verbose = False
+
+    def __init__(self, verbose=True, host_config_file=default_host_config):
+
+        super().__init__(verbose, host_config_file)
+
+        self.verbose = verbose
+        # read hosts
+        with open(host_config_file) as hcfgf:
+            hosts = json.load(hcfgf)
+        self.hosts = hosts
